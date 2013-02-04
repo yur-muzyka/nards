46cc491fcefdc3453407ff0832950ab5a9a73940
Yura <yur.muzyka@gmail.com>
2013-02-04 18:36:00 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nards::Application.config.secret_token = 'aa0ff4d2460ffe7819d103b976d84988495fdb3be944d68c9f4d85305f8f5fcea5f27775139d4f71c20c5e2732ded9149227faed1fed21294b79240adfd58de8'
