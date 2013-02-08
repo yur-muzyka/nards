8d848a97a2a0d412d19ef2210c710a45a85116ad
Yura <yur.muzyka@gmail.com>
2013-02-08 02:30:19 +0200

    chat in progress 80%

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,16 +1,19 @@
 class ChatController < SecurityController
-
   def chat_save
-	@user_id = current_user.id
-	@chat = Chat.new(params[:chat].merge(:user_id => current_user.id))
-	@chat.save
-	redirect_to(:back)
-   end
-   
-   def chat_load
-        @chat3 = Chat.find(:all)
-		@user3 = User.find(:all)
-		render :template => 'chat/chat_load'
+    @user_id = current_user.id
+    @chat = Chat.new(params[:chat].merge(:user_id => current_user.id))
+    @chat.save
+    redirect_to(:back)
+  end
 
-   end
+  def chat_load
+    @chat3 = Chat.find(:all, :conditions => ['id > ?', params[:last]])
+
+    if @chat3.length == 0
+      render :nothing => true
+    else
+      @last_id = @chat3.last[:id]
+      render :template => 'chat/chat_load'
+    end
+  end
 end
