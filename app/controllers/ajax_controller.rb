af6535bde8471a36eedca00602ab768ddf8a396c
Yura <yur.muzyka@gmail.com>
2013-02-12 14:33:49 +0200

    fixe heroku postgresql query bug

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -4,9 +4,20 @@ class AjaxController < ApplicationController
     @chat_message = Chat.find(:all, :conditions => ['id > ?', params[:last]])
     @online_users= User.find(:all, :conditions => ['last_visit > ?', Time.now - 15.seconds], :order => 'username')
     
-    @created_games = Game.joins(:users).group(:game_id).having("COUNT(*) = 1")
+  # @created_games = Game.joins(:users).group(:game_id).having("COUNT(*) = 1")
   # @created_games = Game.joins(:users).group("users.id").having("count(users.id) = 1")  
   # @created_games = Game.joins(:users).where("(select count(users.game_id) from users users2 where users2.game_id = games.id) = 1")
+    @created_games = []
+    @all_games = Game.find(:all)
+    @all_games.each do |game|
+      if game.users.length == 1
+        @created_games << game
+      end
+    end 
+    
+    
+    
+    
     
     render :layout => false, :template => 'ajax/load'
   end
